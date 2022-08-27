37323370119f19cf4e76ce63bb6e92ba11667c8e
gao634 <gao634@purdue.edu>
2022-08-27 12:16:43 -0400

    create pages for the links to go to

--- js-tutorial/app.js
+++ js-tutorial/app.js
@@ -9,7 +9,11 @@ menu.addEventListener('click', function() {
 function change_height() {
     var main_image = document.getElementById('main__image__container');
     var main_container = document.getElementById('main__container');
-    img_height = main_image.style.height;
-    print(img_height);
-    main_container.style.height = '1000px';
+    img_height = main_image.offsetHeight;
+    string_height = img_height.toString() + 'px';
+    main_container.style.height = string_height;
 }
+change_height();
+$(window).resize(function() {
+    change_height();
+  });
