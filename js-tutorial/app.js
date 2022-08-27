9557035bc5446930d054326abde531be3dde6d09
gao634 <gao634@purdue.edu>
2022-08-27 00:05:28 -0400

    figuring out how to fix the image with js

--- js-tutorial/app.js
+++ js-tutorial/app.js
@@ -5,3 +5,12 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 })
+
+function change_height() {
+    var main_image = document.getElementById('main__image__container');
+    var main_container = document.getElementById('main__container');
+    img_height = main_image.style.height;
+    print(img_height);
+    main_container.style.height = '1000px';
+}
+change_height()
